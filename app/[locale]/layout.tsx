726152b572d31f7cf02cc881a8ad53ba852aeb96
Daniel J <d4niel.djm@gmail.com>
2026-01-22 01:27:48 -0500

    Fix card for preview

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -64,7 +64,7 @@ export async function generateMetadata({
       card: "summary_large_image",
       title,
       description,
-      images: [`/card.png`],
+      images: [`${BASE_URL}/card.png`],
     },
     robots: {
       index: true,
